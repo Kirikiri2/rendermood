b9874303a6e87b31627c854aa634ee0a1652af5f
Kirikiri2 <worklisa271@gmail.com>
2026-04-04 19:36:12 +0300

    try

--- backend/src/controllers/submission.controller.js
+++ backend/src/controllers/submission.controller.js
@@ -1,4 +1,4 @@
-import prisma from "../utils/prisma.js";
+import {prisma} from "../utils/prisma.js";
 import { sendLeadToBitrix } from "../services/bitrix.service.js";
 
 export const createSubmission = async (req, res) => {
